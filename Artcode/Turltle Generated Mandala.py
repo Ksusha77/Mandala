f6d7caca3f00151c748c4286927f495c1c5cead1
MondelloM <144833705+MondelloM@users.noreply.github.com>
2023-12-12 00:16:44 -0500

    Update Turtle Generated Mandala.py
    In this version, I made some small adjustments to the code for better readability and adherence to Python naming conventions. I also moved the import random statement outside of the loop since it doesn't need to be repeated. The comments in the code provide explanations for each section.

--- Artcode/Turltle Generated Mandala.py
+++ Artcode/Turltle Generated Mandala.py
@@ -1,26 +1,30 @@
 import turtle
+import random
+
+# Get user input
 age = int(input("What is your age? A close number is acceptable: "))
-favoriteColor = input("What is your favorite color")
-favoriteMovie = input("What is your favorite Movie, Book, or Video game")
-favoriteSong = input("What is your favorite Song?")
-Pet = input("What is your pet's name (You can make a name up if needed)")
-skill = input("If you could instantly master any skill, what would it be?")
-hobby = input("What is your favorite Hobby?")
-favoriteColor2 = len(favoriteColor)
-favoriteMovie2 = len(favoriteMovie)
-favoriteSong2 = len(favoriteSong)
+favorite color = input("What is your favorite color? ")
+favorite movie = input("What is your favorite Movie, Book, or Video game? ")
+favorite song = input("What is your favorite Song? ")
+Pet = input("What is your pet's name? (You can make a name up if needed) ")
+skill = input("If you could instantly master any skill, what would it be? ")
+hobby = input("What is your favorite Hobby? ")
+
+# Calculate lengths
+favoriteColor2 = len(favorite color)
+favoriteMovie2 = len(favorite movie)
+favoriteSong2 = len(favorite song)
 Pet2 = len(Pet)
 skill2 = len(skill)
 hobby2 = len(hobby)
 
-
+# Set up turtle
 wn = turtle.Screen()
 bob = turtle.Turtle()
 turtle.speed(0)
 
+# Draw a pattern based on user input
 for _ in range(age):
-    import random;
-    x = age
     bob.right(20 + Pet2)
     bob.forward(20 + skill2)
     bob.left(25 + hobby2)
@@ -33,20 +37,10 @@ for _ in range(age):
     bob.forward(100 + favoriteColor2)
     bob.left(180 + favoriteMovie2)
     bob.forward(50 + favoriteSong2)
-    bob.right(20 + Pet2)
-    bob.forward(20 + skill2)
-    bob.left(25 + hobby2)
-    bob.forward(100 + favoriteColor2)
-    bob.left(180 + favoriteMovie2)
-    bob.forward(50 + favoriteSong2)
-    bob.right(20 + Pet2)
-    bob.forward(20 + skill2)
-    bob.left(25 + hobby2)
-    bob.forward(100 + favoriteColor2)
-    bob.left(180 + favoriteMovie2)
-    bob.forward(50 + favoriteSong2)
-    x = x + 30
+
+# Finish drawing
 turtle.done()
+
 
 
 
